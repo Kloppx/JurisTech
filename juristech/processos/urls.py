e119f9eae7fbf972af5b07b585458a58313d464d
Victor Calheiros <nchvictor92@gmail.com>
2025-05-27 13:17:46 -0300

    more fix

--- juristech/processos/urls.py
+++ juristech/processos/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('listar/', views.listar_processos, name='listar_processos'),
     path('upload/<int:processo_id>/', views.upload_documento, name='upload_documento'),
     path('detalhar/<int:processo_id>/', views.detalhar_processo, name='detalhar_processo'),
-    path('editar/<int:processo_id>/', views.editar_processo, name='editar_processo'),
+   path('editar/<int:processo_id>/', views.editar_processo, name='editar_processo'),
 ]
